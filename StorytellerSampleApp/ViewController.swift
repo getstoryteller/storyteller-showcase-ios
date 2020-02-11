94f5f35da42e8ef59df61503081125f07cfec47e
jxie93 <zhihao420@hotmail.com>
2020-02-11 15:10:03 +0000

    Add delegate implementation and pull to refresh

--- StorytellerSampleApp/ViewController.swift
+++ StorytellerSampleApp/ViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 import Storyteller
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, StorytellerRowViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var storytellerRowView: StorytellerRowView!
+    
+    var refresher: UIRefreshControl?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        refresher = UIRefreshControl()
+        refresher?.addTarget(self, action: #selector(onPullToRefresh), for: .valueChanged)
+        if #available(iOS 10.0, *) {
+            scrollView.refreshControl = refresher
+        } else {
+            print("\(String(describing: self)) - pull to refresh not supported, reason: iOS 10 or above required")
+        }
+        
+        storytellerRowView.delegate = self
+    }
+    
+    @objc func onPullToRefresh() {
+        storytellerRowView.reloadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,5 +44,19 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         storytellerRowView.reloadData()
     }
+    
+    func onChannelsDataLoadStarted() {
+        NSLog("onChannelsDataLoadStarted")
+    }
+    
+    func onChannelsDataLoadComplete(success: Bool, error: Error?, dataCount: Int) {
+        NSLog("onChannelsDataLoadComplete success \(success), error \(error), dataCount \(dataCount)")
+        refresher?.endRefreshing()
+    }
+    
+    func onChannelDismissed() {
+        NSLog("onChannelDismissed")
+    }
+    
 }
 
