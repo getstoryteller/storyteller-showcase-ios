579141d5611e98766f7f078820a88fd34d42f30f
Michał Januszewski <blackrainpl@gmail.com>
2020-05-18 21:53:20 +0200

    Renamed StorytellerSDK-iOS to StorytellerSDK.

---
+++ XCFrameworks-Sample/StorytellerSampleApp/Libraries/StorytellerSDK.xcframework/ios-armv7_arm64/StorytellerSDK.framework/Headers/StorytellerSDK.h
@@ -0,0 +1,20 @@
+//
+//  StorytellerSDK.h
+//  StorytellerSDK
+//
+//  Created by Jason Xie on 08/07/2019.
+//  Copyright © 2019 Storm Ideas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StorytellerSDK.
+FOUNDATION_EXPORT double StorytellerSDKVersionNumber;
+
+//! Project version string for StorytellerSDK.
+FOUNDATION_EXPORT const unsigned char StorytellerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorytellerSDK/PublicHeader.h>
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+
+
