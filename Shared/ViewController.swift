248f3eba92fcb365d44c1ccc8a1b02113336ff65
Michał Januszewski <blackrainpl@gmail.com>
2021-02-11 22:53:10 +0100

    Added docs for methods.

--- Shared/ViewController.swift
+++ Shared/ViewController.swift
@@ -17,8 +17,7 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+
         refresher = UIRefreshControl()
         refresher?.addTarget(self, action: #selector(onPullToRefresh), for: .valueChanged)
         if #available(iOS 10.0, *) {
@@ -26,8 +25,12 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
         } else {
             print("\(String(describing: self)) - pull to refresh not supported, reason: iOS 10 or above required")
         }
-        
+
+        // Set current class as Storyteller delegate.
         storytellerRowView.delegate = self
+
+        // Set thumbnail shape.
+        // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowView#storytellerrowviewcelltype
         storytellerRowView.cellType = StorytellerRowViewCellType.Square.rawValue
     }
     
@@ -38,8 +41,16 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        // SDK initialization requires providing api key.
+        // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/GettingStarted#sdk-initialization
         Storyteller.sharedInstance.initialize(apiKey: "<apiKey>", onComplete: {
+
+            // Authenticate a user by setting details containing an UUID.
+            // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/Users
             Storyteller.sharedInstance.setUserDetails(userInput: UserInput(externalId: "user-id"))
+
+            // Reload data with the params set above.
+            // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowView#reloaddata
             self.storytellerRowView.reloadData()
         }) { error in
             // Handle error
@@ -48,13 +59,19 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+
         storytellerRowView.reloadData()
     }
-    
+
+    // MARK: - StorytellerRowViewDelegate methods
+
+    // Called when the network request to load data for all stories has started.
     func onStoriesDataLoadStarted() {
         NSLog("onStoriesDataLoadStarted")
     }
 
+    // Called when data loading is finished.
+    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#error-handling
     func onStoriesDataLoadComplete(success: Bool, error: Error?, dataCount: Int) {
         NSLog("onStoriesDataLoadStarted - sucess: \(success), error: \(error), dataCount: \(dataCount).")
 
@@ -63,20 +80,33 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
         }
     }
 
+    // Called when any story has been dismissed.
     func onStoryDismissed() {
         NSLog("onStoryDismissed")
     }
 
+    // Called when analytics event occurs.
+    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#analytics
     func onUserActivityOccurred(type: UserActivity.EventType, data: UserActivityData) {
         NSLog("onUserActivityOccurred - type: \(type), data: \(data).")
     }
 
+    // Called when user swipes up on story's page.
+    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#swiping-up-to-the-integrating-app
     func userSwipedUpToApp(swipeUpUrl: String) {
         NSLog("userSwipedUpToApp")
     }
 
+    // Called when tile with given index becomes visible.
+    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#tile-visibility
     func tileBecameVisible(index: Int) {
         NSLog("tileBecameVisible: \(index)")
+    }
+
+    // Called when tenant is configured to use ads from the containing app.
+    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#client-ads
+    func getAdsForRow(stories: [ClientStory], onComplete: @escaping ([ClientAd?]) -> Void, onError: @escaping (Error) -> Void) {
+        onComplete([])
     }
 }
 
