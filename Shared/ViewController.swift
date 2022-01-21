f49cf02ec0209308b2c935e82113a3a8d8d9861e
bartoszstelmaszuk <stelmaszuk.b@gmail.com>
2022-01-21 10:54:40 +0100

    update to Storyteller SDK 6.0.0 (#25)

--- Shared/ViewController.swift
+++ Shared/ViewController.swift
@@ -9,48 +9,56 @@
 import UIKit
 import StorytellerSDK
 
-class ViewController: UIViewController, StorytellerRowViewDelegate {
+class ViewController: UIViewController, StorytellerListViewDelegate, StorytellerDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var storytellerRowView: StorytellerRowView!
+    @IBOutlet weak var storytellerGridView: StorytellerGridView!
     
     var refresher: UIRefreshControl?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         refresher = UIRefreshControl()
         refresher?.addTarget(self, action: #selector(onPullToRefresh), for: .valueChanged)
-        if #available(iOS 10.0, *) {
-            scrollView.refreshControl = refresher
-        } else {
-            print("\(String(describing: self)) - pull to refresh not supported, reason: iOS 10 or above required")
-        }
-
-        // Set current class as Storyteller delegate.
+        scrollView.refreshControl = refresher
+        
+        // Set current class for StorytellerDelegate.
+        // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-delegate#HowToUse
+        Storyteller.sharedInstance.delegate = self
+              
+        // Set current class for StorytellerListViewDelegates.
+        // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-list-view-delegate#HowToUse
         storytellerRowView.delegate = self
+        storytellerGridView.delegate = self
 
         // Set thumbnail shape.
-        // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowView#storytellerrowviewcelltype
-        storytellerRowView.cellType = StorytellerRowViewCellType.Square.rawValue
+        // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-list-view#Attributes
+        storytellerRowView.cellType = StorytellerListViewCellType.square.rawValue
+        
+        // Set left inset value.
+        // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-row-view#Attributes
+        storytellerRowView.insetStart = 4.0
     }
     
     @objc func onPullToRefresh() {
         storytellerRowView.reloadData()
+        storytellerGridView.reloadData()
     }
 
     @IBAction func changeUserTapped() {
         setRandomUser()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         // SDK initialization requires providing api key.
-        // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/GettingStarted#sdk-initialization
+        // For more info, see: https://www.getstoryteller.com/documentation/ios/getting-started#SDKInitialization
         Storyteller.sharedInstance.initialize(apiKey: "<apiKey>", userInput: UserInput(externalId: "user-id"), onComplete: {
             // Reload data with the params set above.
-            // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowView#reloaddata
+            // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-list-view#reloadData
             self.storytellerRowView.reloadData()
+            self.storytellerGridView.reloadData()
         }) { error in
             // Handle error
         }
@@ -74,15 +82,16 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
         // If you use login in your app and wish to allow users to logout and log back in as a new user
         // (or proceed as an anonymous user) then when a user logs out you should initialize SDK
         // again specifying a new externalId. Note that this will reset the local store of which pages the user has viewed.
-        // For more info, see - https://docs.getstoryteller.com/documents/ios-sdk/Users
+        // For more info, see - https://www.getstoryteller.com/documentation/ios/users
         let newIdentifier = UUID().uuidString
         
         // SDK initialization requires providing api key.
-        // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/GettingStarted#sdk-initialization
+        // For more info, see: https://www.getstoryteller.com/documentation/ios/getting-started#SDKInitialization
         Storyteller.sharedInstance.initialize(apiKey: "<apiKey>", userInput: UserInput(externalId: newIdentifier), onComplete: {
             // Reload data with the params set above.
-            // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowView#reloaddata
+            // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-list-view#reloadData
             self.storytellerRowView.reloadData()
+            self.storytellerGridView.reloadData()
         }) { error in
             // Handle error
         }
@@ -109,7 +118,7 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
     }
 
     // Called when data loading is finished.
-    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#error-handling
+    // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-list-view-delegate#ErrorHandling
     func onStoriesDataLoadComplete(success: Bool, error: Error?, dataCount: Int) {
         NSLog("onStoriesDataLoadStarted - sucess: \(success), error: \(error), dataCount: \(dataCount).")
 
@@ -124,28 +133,28 @@ class ViewController: UIViewController, StorytellerRowViewDelegate {
     }
 
     // Called when analytics event occurs.
-    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#analytics
+    // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-delegate#Analytics
     func onUserActivityOccurred(type: UserActivity.EventType, data: UserActivityData) {
         NSLog("onUserActivityOccurred - type: \(type), data: \(data).")
     }
 
     // Called when user swipes up on story's page.
-    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#swiping-up-to-the-integrating-app
+    // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-delegate#SwipingUpToTheIntegratingApp
     func userSwipedUpToApp(swipeUpUrl: String) {
         // Open another module in the app and pass given url as param.
-        storytellerRowView.dismissStoryView(animated: true, dismissReason: nil) {
+        Storyteller.dismissStoryView(animated: true, dismissReason: nil) {
             self.performSegue(withIdentifier: "showDeeplinkPreview", sender: swipeUpUrl)
         }
     }
 
     // Called when tile with given index becomes visible.
-    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#tile-visibility
+    // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-list-view-delegate#TileVisibility
     func tileBecameVisible(index: Int) {
         NSLog("tileBecameVisible: \(index)")
     }
 
     // Called when tenant is configured to use ads from the containing app.
-    // For more info, see: https://docs.getstoryteller.com/documents/ios-sdk/StorytellerRowViewDelegate#client-ads
+    // For more info, see: https://www.getstoryteller.com/documentation/ios/storyteller-delegate#ClientAds
     func getAdsForRow(stories: [ClientStory], onComplete: @escaping ([ClientAd?]) -> Void, onError: @escaping (Error) -> Void) {
         onComplete([])
     }
