e3c2f328128fb632c88cd64191bba1d65e40f9f2
Adam Lipka <adam.lipka@gmail.com>
2024-03-01 10:54:08 +0100

    Updated SDK to 9.10.0

--- main/Sample App/Api/Endpoints.swift
+++ main/Sample App/Api/Endpoints.swift
@@ -2,33 +2,33 @@ import Foundation
 
 struct ValidateCodeEndpoint: Endpoint {
     typealias Response = TenantConfigResponse
-    let path = "/validateCode"
+    let path = "/v2/validateCode"
     let method: HTTPMethod = .POST
 }
 
 struct SettingsEndpoint: Endpoint {
     typealias Response = TenantConfigResponse
-    let path = "/settings"
+    let path = "/v2/settings"
 }
 
 struct HomeFeedEndpoint: Endpoint {
     typealias Response = FeedResponse
-    let path = "/home"
+    let path = "/v2/home"
 }
 
 struct TeamsEndpoint: Endpoint {
     typealias Response = FavoriteTeamsResponse
-    let path = "/teams"
+    let path = "/v2/teams"
 }
 
 struct LanguagesEndpoint: Endpoint {
     typealias Response = LanguagesResponse
-    let path = "/languages"
+    let path = "/v2/languages"
 }
 
 struct TabsEndpoint: Endpoint {
     typealias Response = TabsResponse
-    let path = "/tabs"
+    let path = "/v2/tabs"
 }
 
 struct TabByIdEndpoint: Endpoint {
